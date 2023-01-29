22de1ce0df4bbd5084b33b75e1403cacc96dfe64
Kolomnin <kolomninsp@gmail.com>
2023-01-29 20:48:48 +0300

    Fifth commit. Добавление Avatar.

--- src/main/java/ru/hogwarts/school/repositories/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repositories/StudentRepository.java
@@ -1,11 +1,13 @@
 package ru.hogwarts.school.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.model.Student;
 
 import java.util.Collection;
 import java.util.List;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
     List<Student> findByAge(int age);
 
@@ -14,3 +16,4 @@ public interface StudentRepository extends JpaRepository<Student,Long> {
     Collection<Student> findStudentsById(Long id);
 
 }
+
